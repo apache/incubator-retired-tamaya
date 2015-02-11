c7f85a53a9ca38855dcd59fd15788c8809c7201c
Oliver B. Fischer <plexus@apache.org>
2015-02-11 19:01:49 +0100

    TAMAYA-39 Added default property source provider for JSON based property sources. TAMAYA-65 Started to use Arquillian for integration tests.

---
+++ modules/json/src/main/java/org/apache/tamaya/modules/json/JSONPropertySourceProvider.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.tamaya.modules.json;
+
+import org.apache.tamaya.ConfigException;
+import org.apache.tamaya.spi.PropertySource;
+import org.apache.tamaya.spi.PropertySourceProvider;
+
+import java.net.URL;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Provides all <a href="http://json.org">JSON</a>
+ * property sources found in the  classpath
+ * in {@code META-INF/javaconfiguration.json}.
+ *
+ * @see PropertySourceProvider
+ */
+public class JSONPropertySourceProvider implements PropertySourceProvider {
+    public final static String DEFAULT_RESOURCE_NAME = "javaconfiguration.json";
+
+    @Override
+    public Collection<PropertySource> getPropertySources() {
+        List<PropertySource> sources;
+
+        try {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            Enumeration<URL> urls = classLoader.getResources("META-INF/" + DEFAULT_RESOURCE_NAME);
+
+            sources = Collections.list(urls)
+                                 .stream()
+                                 .map(JSONPropertySource::new)
+                                 .collect(toList());
+
+        } catch (Exception e) {
+            String msg = "Failure while loading JSON property sources.";
+
+            throw new ConfigException(msg, e);
+        }
+
+        return sources;
+    }
+}
