cf6b4118fc624e54c4d118080c2916e5445825a3
anatole <anatole@apache.org>
2015-11-30 08:29:58 +0100

    Added SPI for sending events for beans configured (used e.g. for documentation and logging).

---
+++ modules/injection-api/src/main/java/org/apache/tamaya/inject/spi/ConfiguredMethod.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.tamaya.inject.spi;
+
+import org.apache.tamaya.ConfigException;
+import org.apache.tamaya.Configuration;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
+
+/**
+ * Abstract model of an method used to inject configuration.
+ */
+public interface ConfiguredMethod {
+
+    /**
+     * Get the key required to be evaluated.
+     * @return the configured keys.
+     */
+    Collection<String> getConfiguredKeys();
+
+    /**
+     * Get the methods input parameter types.
+     * @return
+     */
+    Class<?>[] getParameterTypes();
+
+    /**
+     * Get the underlying method reflection type.
+     * @return the method element.
+     */
+    Method getAnnotatedMethod();
+
+    /**
+     * Get the method's name, e.g. {@code setName}.
+     * @return the name, never null.
+     */
+    String getName();
+
+    /**
+     * Get the methods signature, e.g. {@code void setName(String)}.
+     * @return he signature, never null.
+     */
+    String getSignature();
+
+    /**
+     * This method actually configures the given method on a instance of its parent type.
+     * This evaluates the initial key closure and applies changes on the field.
+     *
+     * @param instance the target instance, not null.
+     * @param config the configuration, not null.
+     * @throws ConfigException if evaluation or conversion failed.
+     */
+    void configure(Object instance, Configuration config);
+}
---
+++ modules/injection-api/src/main/java/org/apache/tamaya/inject/spi/ConfiguredType.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.tamaya.inject.spi;
+
+import org.apache.tamaya.Configuration;
+
+import java.util.Collection;
+
+/**
+ * Abstract model of an type used to inject configuration. This also includes instances passed programmatically.
+ */
+public interface ConfiguredType {
+
+    /**
+     * Get the type's class.
+     * @return
+     */
+    Class getType();
+
+    /**
+     * Get the type's name.
+     * @return the type's name.
+     */
+    String getName();
+
+    /**
+     * Get the registered configured fields.
+     * @return the registered configured fields, never null.
+     */
+    public Collection<ConfiguredField> getConfiguredFields();
+
+    /**
+     * Get the registered configured methods.
+     * @return the registered configured methods, never null.
+     */
+    Collection<ConfiguredMethod> getConfiguredMethods();
+
+    /**
+     * This method actually configures an instance using the given configuration data.
+     *
+     * @param instance The instance to be configured, not null.
+     * @param config  the target config, not null.
+     */
+    void configure(Object instance, Configuration config);
+
+
+}
